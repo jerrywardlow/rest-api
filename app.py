dd2c7666c68f888074f1d158441f55fefb4cc307
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-06-09 19:37:08 -0700

    Really stupid typo

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_tasks():
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
-        about(404)
+        abort(404)
     return jsonify({'task': task[0]})
 
 

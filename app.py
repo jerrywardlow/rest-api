4247e26185313b29602fc233d50e78fb15d87e4d
jerrywardlow <jerrylikesbikes@gmail.com>
2016-06-09 22:53:24 -0700

    PUT route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!env/bin/python
 from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
@@ -44,6 +44,26 @@ def create_task():
     tasks.append(task)
     return jsonify({'task': task}), 201
 
+# PUT routes
+@app.route('/todo/api/v1/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) != unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+# Error handlers
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)

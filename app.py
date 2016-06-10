a31d594e6e3f17affc4c07cc4bd5f55e38b0c393
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-06-09 19:29:32 -0700

    Basic API for task list

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'descripttion': u'Paper, paper products, staples, staplers, more paper',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Must read everything Miguel has written..',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
     app.run(debug=True)

97216ea6b6ead1105f4e90f652886da7e2906066
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-06-10 09:49:17 -0700

    Updated tasks route with URI on return

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!env/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -18,10 +18,19 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 # GET routes
 @app.route('/todo/api/v1/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):

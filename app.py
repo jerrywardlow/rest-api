e3863c74d5ca2fd8b62913a8072a8325ea2bc5cf
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-06-09 19:34:01 -0700

    Route for individual task

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -21,6 +21,13 @@ tasks = [
 @app.route('/todo/api/v1/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        about(404)
+    return jsonify({'task': task[0]})
 
 
 if __name__ == '__main__':

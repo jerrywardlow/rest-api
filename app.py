b08fa52aeab560cce8e27a093e615741a580599b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-06-09 19:52:19 -0700

    POST route for new task

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,7 @@ tasks = [
     }
 ]
 
+# GET routes
 @app.route('/todo/api/v1/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
@@ -28,6 +29,20 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+# POST routes
+@app.route('/todo/api/v1/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 @app.errorhandler(404)
 def not_found(error):
